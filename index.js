49476293f6e6e9120a670c24e40619d5daeca4dd
Your Name <you@example.com>
2025-12-19 19:20:03 +0100

    all files

---
+++ index.js
@@ -0,0 +1,15 @@
+// Add AND DELET
+
+//document.getElementById("").addEventListener - ("click", function() { for (of) })
+//confirmOrder
+const modal = document.getElementById("confirmModal");
+const confirmBtn = document.getElementById("confirmOrder"); // زر تأكيد الطلب
+
+confirmBtn.addEventListener("click", () => {
+    modal.style.display = "flex";
+});
+
+closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+confirmBtn.addEventListener("click", function() { doun })
